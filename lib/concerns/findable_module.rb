af26a9425bb588288722b396612a3cbd21cd883e
Mark Stabler <mark@markstabler.com>
2020-03-30 18:27:59 +0000

    Automatically backed up by Learn

--- lib/concerns/findable_module.rb
+++ lib/concerns/findable_module.rb
@@ -1,6 +1,6 @@
 module Concerns::Findable
-  def self.find_by_name(name)
-    self.all.detect{ |inst| inst.name == name }
+  def find_by_name(name)
+    all.detect{ |inst| inst.name == name }
   end
 
 #  def self.find_or_create_by_name(name)
