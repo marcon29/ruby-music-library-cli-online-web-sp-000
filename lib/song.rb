acde4093f51797d3364cc961fa52c1171590e3b3
Mark Stabler <mark@markstabler.com>
2020-03-30 21:14:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,9 +47,6 @@ class Song
     artist = Artist.find_or_create_by_name(artist_name)
     genre = Genre.find_or_create_by_name(genre_name)
     self.new(song_name, artist, genre)
-
-
-#binding.pry
   end
 
 
