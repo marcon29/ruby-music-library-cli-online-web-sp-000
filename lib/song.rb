fe0564337e115f46537fe20bacbd18545b59958c
Mark Stabler <mark@markstabler.com>
2020-03-30 21:13:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,7 +45,7 @@ class Song
     genre_name = filename.split(" - ")[2].split(".")[0]
 
     artist = Artist.find_or_create_by_name(artist_name)
-    genre = Genre.find_or_create_by_name(artist_name)
+    genre = Genre.find_or_create_by_name(genre_name)
     self.new(song_name, artist, genre)
 
 
