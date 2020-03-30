0a06deac85c30d7718b03140f09a17521ee7cae1
Mark Stabler <mark@markstabler.com>
2020-03-30 19:50:12 +0000

    Automatically backed up by Learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -12,16 +12,6 @@ class MusicImporter
     Dir.children(path)
   end
 
-  def self.new_from_filename
-    self.files.each do |filename|
-      artist_name = filename.split(" - ")[0]
-      song_name = filename.split(" - ")[1]
-      genre_name = filename.split(" - ")[2].split(".")[0]
-
-      Song.new(artist_name)
-    end
-  end
-
   def self.import
   end
 
