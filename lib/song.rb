c0ebcf7bbca6a1a9c021fbc86738a2b873207b4f
Mark Stabler <mark@markstabler.com>
2020-03-30 18:31:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,6 @@
 class Song
+  extend Concerns::Findable
+
   attr_accessor :name, :artist, :genre
 
   @@all = []
@@ -37,17 +39,17 @@ class Song
     new_song
   end
 
-  def self.find_by_name(name)
-    self.all.detect{ |s| s.name == name }
-  end
+#  def self.find_by_name(name)
+#    self.all.detect{ |s| s.name == name }
+#  end
 
-  def self.find_or_create_by_name(name)
-    if self.find_by_name(name)
-      self.find_by_name(name)
-    else
-      self.create(name)
-    end
-  end
+#  def self.find_or_create_by_name(name)
+#    if self.find_by_name(name)
+#      self.find_by_name(name)
+#    else
+#      self.create(name)
+#    end
+#  end
 
 
 
