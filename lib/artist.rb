51baea248da48f2c9d9225a0d6f1f6207f5b738b
Mark Stabler <mark@markstabler.com>
2020-03-30 17:39:02 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -39,7 +39,7 @@ class Artist
   def genres
     # search through song list
     # pull out each genre and add to genre list if not already in it
-binding.pry
+#binding.pry
     all_genres = []
     self.songs.each do |s|
       if !all_genres.include?(s.genre)
