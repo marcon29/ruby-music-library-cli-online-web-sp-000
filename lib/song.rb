4c913e2c4301693f7ee495aa5cea4924ae8a3b72
Mark Stabler <mark@markstabler.com>
2020-03-30 18:20:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,12 +23,12 @@ class Song
     @@all
   end
 
-  def self.destroy_all
-    @@all.clear
-  end
-
   def save
     @@all << self
+  end
+
+  def self.destroy_all
+    @@all.clear
   end
 
   def self.create(name)
