b51cba6022eb675e47fec15245250d7dcb843ab3
Mark Stabler <mark@markstabler.com>
2020-03-30 21:54:53 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -20,4 +20,22 @@ class MusicLibraryController
     self.call unless input == "exit"
   end
 
+  def list_songs
+  end
+  
+  def list_artists
+  end
+  
+  def list_genres
+  end
+  
+  def list_songs_by_artist
+  end
+  
+  def list_songs_by_genre
+  end
+  
+  def play_song
+  end
+
 end
