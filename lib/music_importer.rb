ced05ee82b8118814daaed56e700049c0123c7b3
Mark Stabler <mark@markstabler.com>
2020-03-30 19:50:17 +0000

    Automatically backed up by Learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -4,7 +4,6 @@ class MusicImporter
   # each instance is a directory of music files
   def initialize(path)
     @path = path
-    #binding.pry
   end
 
 # puts all files in directory at @path in an array
