fadbb8266e9ea443eb41bfaa977ef6eb2e0117d1
Mark Stabler <mark@markstabler.com>
2020-03-30 17:46:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,10 +43,12 @@ class Song
     new_song
   end
 
-  def self.find_by_name
+  def self.find_by_name(name)
+    
   end
 
-  def self.find_or_create_by_name
+  def self.find_or_create_by_name(name)
+    
   end
 
 
