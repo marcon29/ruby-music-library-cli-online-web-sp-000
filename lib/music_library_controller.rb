0104cd1188b0bfd9b892f8beef2be959c099758b
Mark Stabler <mark@markstabler.com>
2020-03-31 02:18:32 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -70,14 +70,15 @@ class MusicLibraryController
 
   def play_song
     puts "Which song number would you like to play?"
+    list_songs
     input = gets.strip.to_i
-    if !input.between?(1, library.count)
-      play_song
-    else
-      song = list_songs[input-1].name#.split(" - ")[1]
-      artist = list_songs[input-1].artist.name#split(" - ")[0]
-      puts "Playing #{song} by #{artist}"
-    end
+#    if !input.between?(1, library.count)
+#      play_song
+#    else
+#      song = list_songs[input-1].name#.split(" - ")[1]
+#      artist = list_songs[input-1].artist.name#split(" - ")[0]
+#      puts "Playing #{song} by #{artist}"
+#    end
 #binding.pry
   end
 
