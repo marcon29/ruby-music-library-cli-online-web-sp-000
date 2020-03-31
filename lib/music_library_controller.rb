6780dd33a33652a6c1d3da3b2396a99b7c88c90f
Mark Stabler <mark@markstabler.com>
2020-03-31 02:11:21 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -25,12 +25,20 @@ class MusicLibraryController
   end
 
   def list_songs
+    #first try
     #sorted_lib = library.sort_by { |s| s.scan(/- .+ -/) }
     #sorted_lib.each_with_index { |s, i| puts "#{i+1}. #{s.chomp(".mp3")}"}
 
-    lib = Song.all.collect { |s| "#{s.artist.name} - #{s.name} - #{s.genre.name}" }
-    sorted_lib = lib.sort_by { |s| s.scan(/- .+ -/) }
-    sorted_lib.each_with_index { |n, i| puts "#{i+1}. #{n}"}
+    #second try
+    #lib = Song.all.collect { |s| "#{s.artist.name} - #{s.name} - #{s.genre.name}" }
+    #sorted_lib = lib.sort_by { |s| s.scan(/- .+ -/) }
+    #sorted_lib.each_with_index { |n, i| puts "#{i+1}. #{n}"}
+
+    # school solution
+    Song.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
+      puts "#{i}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
+    end
+
 binding.pry
   end
 
