6e01c64dfe1596381a661f5cace9d28e5333da79
Mark Stabler <mark@markstabler.com>
2020-03-31 02:41:23 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -70,7 +70,6 @@ class MusicLibraryController
 
   def play_song
     puts "Which song number would you like to play?"
-    list_songs
     input = gets.strip.to_i
 
     if input.between?(1, Song.all.count)
